c548c326ef64f28a9a44f424f5bc2bf81ed66688
surya-k-soni <surya_creation@outlook.com>
2025-06-04 21:01:15 -0400

    Update hello_world.php

--- hello_world.php
+++ hello_world.php
@@ -2,4 +2,16 @@
 
   echo "Hello World!";
   echo "This is my first GitHub commit.";
+
+  echo "Hello, Surya! 🎉 Your PHP environment is up and running.";
+  echo "<br>";
+    /*
+      echo phpversion();
+      echo "<br>";
+      echo phpinfo();
+      echo "<br>";
+    */
+
+    $today = date("l, F j, Y");
+    echo "Happy coding, Surya! Today is $today.";
 ?>
