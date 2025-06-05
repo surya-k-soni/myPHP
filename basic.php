3524b58beac44e2772ec3c569fecb83572945f9e
surya-k-soni <surya_creation@outlook.com>
2025-06-04 20:59:55 -0400

    Add files via upload

---
+++ basic.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <title>PHP Basics</title>
+    </head>
+    <body>
+        <h1>Welcome to PHP Basics</h1>
+        
+        <?php
+            echo "<p>This paragraph is rendered by PHP.</p>";
+        ?>
+    </body>
+</html>
